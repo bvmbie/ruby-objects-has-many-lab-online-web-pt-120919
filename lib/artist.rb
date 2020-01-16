7cf76d42e7c4cbd66f9dbc26b2a0c3ec5362f44f
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-16 20:53:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,7 @@ class Artist
   
   def add_song_by_name(song_name)
     singer = Song.new(song_name)
-    add_song(song_name)
+    add_song(singer)
     
     #song = Song.new(name)
     #add_song(song)
