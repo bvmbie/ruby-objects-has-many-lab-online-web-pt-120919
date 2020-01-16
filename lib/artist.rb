6714010e7352f815b040b324488e794fc297ec21
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-16 20:57:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,11 +2,8 @@
 class Artist
   attr_accessor :name, :songs
 
-  #@@song_count = 0
-
   def initialize(name)
     @name = name
-    #@songs = []
   end
   
   def songs
@@ -17,21 +14,12 @@ class Artist
   
   def add_song(song)
     song.artist = self
-    #self.songs << song
-    #@@song_count =+ 1
   end
   
   def add_song_by_name(song_name)
     singer = Song.new(song_name)
     add_song(singer)
-    
-    #song = Song.new(name)
-    #add_song(song)
-    #self.songs << song
-    #@@song_count =+ 1
   end
-
- 
 
   def self.song_count
     Song.all.count
